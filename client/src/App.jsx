8bc6766c7b733ccf34c19eacbcc08c22c6e07f4b
Afnan <afnanyb@gmail.com>
2023-12-23 14:46:25 -0800

    sign in input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,49 @@
-import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  useNavigate,
+  Link,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.scss";
+import { useState } from "react";
 // import Navbar from './components/Navbar';
 import JournalEntry from "./Component/JournalEntry";
+
 // import MoodTracker from './components/MoodTracker';
 // import GoalSetting from './components/GoalSetting';
+function Home() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
+  return (
+    <div>
+      <h1 className="header__sign">Sign In</h1>
+      <label>Name</label>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder=""
+      />
+      <label>Password</label>
+      <input
+        value={password}
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder=""
+      />
+      <button
+        onClick={() => {
+          // TODO:handel sign in
+          navigate("/journal-entry");
+        }}
+      >
+        Sign in{" "}
+      </button>
+    </div>
+  );
+}
 function App() {
   return (
     <Router>
@@ -14,7 +53,7 @@ function App() {
         <Link to="/goals"> Goals </Link>
       </div>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/journal-entry" element={<JournalEntry />} />
         <Route path="/goals" element={<div> Goals</div>} />
       </Routes>

4eeed136e6cd7f65a2fc697df8cc9b1e88ec9424
Afnan <afnanyb@gmail.com>
2023-12-23 14:15:04 -0800

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 // import Navbar from './components/Navbar';
 import JournalEntry from "./Component/JournalEntry";
@@ -8,11 +8,15 @@ import JournalEntry from "./Component/JournalEntry";
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
+      <div>
+        <Link to="/">Home </Link>
+        <Link to="/journal-entry">Journal Entry </Link>
+        <Link to="/goals"> Goals </Link>
+      </div>
       <Routes>
-        {/* <Route path="/" exact component={MoodTracker} /> */}
+        <Route path="/" element={<div>Home</div>} />
         <Route path="/journal-entry" element={<JournalEntry />} />
-        {/* <Route path="/goals" component={GoalSetting} /> */}
+        <Route path="/goals" element={<div> Goals</div>} />
       </Routes>
     </Router>
   );
